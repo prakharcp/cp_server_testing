51e94025c210acb7ffc5b3a20020df81b031ccf2
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2023-03-29 18:03:00 +0530

    V1

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 4000;
+
+app.post("/jwt-testing", (req, res) => {
+  try {
+    res.cookie("jwt", "token", {
+      secure: true,
+      sameSite: "Lax",
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      domain: "www.wmg.com",
+    });
+
+    res.redirect("/done");
+  } catch (err) {
+    console.log(err);
+    res.json({
+      err,
+    });
+  }
+});
+
+app.get("/done", (req, res) => {
+  try {
+    res.json({
+      msg: "success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      err,
+    });
+  }
+});
+
+app.listen(PORT, function () {
+  console.log(`App is running in http://localhost:${PORT}`);
+});
