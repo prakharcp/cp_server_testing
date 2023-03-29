8c9937ab38c39dcc2cd7d16542d4a958e07177d2
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2023-03-29 23:01:33 +0530

    Changes

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
